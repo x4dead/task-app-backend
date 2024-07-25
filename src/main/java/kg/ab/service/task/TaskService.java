0d4f36d9063a1676f399f4bad6a3757136cf1288
x4dead <muratovargen0@gmail.com>
2024-07-25 15:07:38 +0600

    фикс

--- src/main/java/kg/ab/service/task/TaskService.java
+++ src/main/java/kg/ab/service/task/TaskService.java
@@ -7,7 +7,6 @@ import kg.ab.service.task.dto.UpdateTaskReq;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public interface TaskService {
